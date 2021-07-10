ee34eaffcb51ce30d873d0dff948a6aa625321f3
caincoan12 <67595382+caincoan12@users.noreply.github.com>
2021-07-10 21:09:01 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,10 +2,10 @@ import os
 
 # The prefix that will be used to parse commands.
 # It doesn't have to be a single character!
-COMMAND_PREFIX = "!"
+COMMAND_PREFIX = ">n"
 
 # The bot token. Keep this secret!
-BOT_TOKEN = "Get this from https://discord.com/developers/applications"
+BOT_TOKEN = "NzcwODUxNTI2Mzk4MTgxMzk2.X5jlbQ.FFJp9wHKkY3jPTcQe5TrcFIP E"
 
 # The now playing game. Set this to anything false-y ("", None) to disable it
 NOW_PLAYING = COMMAND_PREFIX + "commands"
